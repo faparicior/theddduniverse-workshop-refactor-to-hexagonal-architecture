95115635b89a561bb6ae3e4557f32b5897a5eb02
Fernando Aparicio <faparicior@gmail.com>
2024-03-14 19:42:08 +0100

    Add status to response and aggregate uuid from UI

--- php/src/framework/FrameworkResponse.php
+++ php/src/framework/FrameworkResponse.php
@@ -5,9 +5,9 @@ namespace Demo\App\framework;
 
 final readonly class FrameworkResponse
 {
-    public const STATUS_OK = 200;
-    public const STATUS_CREATED = 201;
-    public const STATUS_BAD_REQUEST = 400;
+    public const int STATUS_OK = 200;
+    public const int STATUS_CREATED = 201;
+    public const int STATUS_BAD_REQUEST = 400;
 
     public function __construct(private int $statusCode, private array $data)
     {
